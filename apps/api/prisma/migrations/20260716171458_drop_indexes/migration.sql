2a679f0fd4b987165aba1b3b0456a92bf9324a42
Phi Vo <sunflynf@gmail.com>
2026-07-19 00:07:29 +0700

    feat: add production release documentation and drop legacy search index migrations

---
+++ apps/api/prisma/migrations/20260716171458_drop_indexes/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX "Collection_searchText_trgm_idx";
+
+-- DropIndex
+DROP INDEX "Cuisine_searchText_trgm_idx";
+
+-- DropIndex
+DROP INDEX "DiningArea_searchText_trgm_idx";
+
+-- DropIndex
+DROP INDEX "RestaurantEntry_searchText_trgm_idx";
+
+-- DropIndex
+DROP INDEX "User_searchText_trgm_idx";
